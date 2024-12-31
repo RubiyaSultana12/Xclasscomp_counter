2f5ee8dbf161d97a299c6c27c69ff35e24d4ac48
RubiyaSultana12 <rubiya.sultana22@gmail.com>
2024-12-31 16:55:44 +0530

    xclass-commit

---
+++ src/CounterApp.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import React from "react";
+
+class CounterApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <div>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CounterApp;
